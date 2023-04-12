5d539238db7fd738f68b057a931a6eeda73e33fe
Santosh J.S <santosh.js@ip-192-168-10-161.ap-south-1.compute.internal>
2023-04-12 19:59:07 +0530

    feat:hello world from retail-app

---
+++ ui/src/sections/auth/login/LoginForm.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// @mui
+import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// components
+import Iconify from '../../../components/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+    const navigate = useNavigate();
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClick = () => {
+        navigate('/dashboard', { replace: true });
+    };
+
+    return (
+        <>
+            <Stack spacing={4}>
+                <TextField name="phone" label="Phone Number" type="number" />
+
+                <TextField
+                    name="password"
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+
+                <LoadingButton
+                    fullWidth
+                    size="large"
+                    margin="normal"
+                    type="submit"
+                    variant="contained"
+                    onClick={handleClick}
+                >
+                    Login
+                </LoadingButton>
+            </Stack>
+        </>
+    );
+}
